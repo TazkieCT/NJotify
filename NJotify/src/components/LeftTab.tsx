49f466e7c6cd1ce0b8a745fa2ba4c0b977a1a9c8
Tazkie <100460377+TazkieCT@users.noreply.github.com>
2024-07-10 16:18:08 +0700

    Homepage update
    Left bar tinggal scroll dan 1 component, player music bar belum selesai

--- NJotify/src/components/LeftTab.tsx
+++ NJotify/src/components/LeftTab.tsx
@@ -2,7 +2,10 @@ import { IoSearch } from "react-icons/io5";
 import { GoHome } from "react-icons/go";
 import style from "../styles/LayoutPage/LeftTab.module.css"
 import { VscLibrary } from "react-icons/vsc";
-import { BsPlusLg } from "react-icons/bs";
+import { FiPlus } from "react-icons/fi";
+import { IoArrowForward } from "react-icons/io5";
+import { LuList } from "react-icons/lu";
+
 
 const LeftTab = () => {
   return (
@@ -12,8 +15,75 @@ const LeftTab = () => {
         <a className={style.links}><span className={style.big}><IoSearch /></span> Search</a>
       </div>
       <div className={style.library}>
-        <a className={style.links}><span className={style.big}><VscLibrary /></span> Your Library</a>
-        <BsPlusLg/>
+        <div className={style['flex-between']}>
+          <a className={style.links}><span className={style.big}><VscLibrary /></span> Your Library</a>
+          <a className={style['lib-button']}>
+            <span className={style.medium}><FiPlus/></span>
+            <span className={style.medium}><IoArrowForward/></span>
+          </a>
+        </div>
+        <div className={style.album}>
+          <div className={`${style['flex-between']} ${style['mb-10']}`}>
+            <a className={style.medium}><IoSearch /></a>
+            <a className={style.recent}>Recents<span className={style.medium}><LuList/></span></a>
+          </div>
+          <div className={style.item}>
+            <img className={style['album-cover']} width={40} src="https://i1.sndcdn.com/artworks-y6qitUuZoS6y8LQo-5s2pPA-t1080x1080.jpg" alt="" />
+            <div className={style.col}>
+              <span className={style['album-title']}>My Playlist</span>
+              <span className={style['album-subtitle']}>Playlist • User</span>
+            </div>
+          </div>
+          <div className={style.item}>
+            <img className={style['album-cover']} width={40} src="https://i1.sndcdn.com/artworks-y6qitUuZoS6y8LQo-5s2pPA-t1080x1080.jpg" alt="" />
+            <div className={style.col}>
+              <span className={style['album-title']}>My Playlist</span>
+              <span className={style['album-subtitle']}>Playlist • User</span>
+            </div>
+          </div>
+          <div className={style.item}>
+            <img className={style['album-cover']} width={40} src="https://i1.sndcdn.com/artworks-y6qitUuZoS6y8LQo-5s2pPA-t1080x1080.jpg" alt="" />
+            <div className={style.col}>
+              <span className={style['album-title']}>My Playlist</span>
+              <span className={style['album-subtitle']}>Playlist • User</span>
+            </div>
+          </div>
+          <div className={style.item}>
+            <img className={style['album-cover']} width={40} src="https://i1.sndcdn.com/artworks-y6qitUuZoS6y8LQo-5s2pPA-t1080x1080.jpg" alt="" />
+            <div className={style.col}>
+              <span className={style['album-title']}>My Playlist</span>
+              <span className={style['album-subtitle']}>Playlist • User</span>
+            </div>
+          </div>
+          <div className={style.item}>
+            <img className={style['album-cover']} width={40} src="https://i1.sndcdn.com/artworks-y6qitUuZoS6y8LQo-5s2pPA-t1080x1080.jpg" alt="" />
+            <div className={style.col}>
+              <span className={style['album-title']}>My Playlist</span>
+              <span className={style['album-subtitle']}>Playlist • User</span>
+            </div>
+          </div>
+          <div className={style.item}>
+            <img className={style['album-cover']} width={40} src="https://i1.sndcdn.com/artworks-y6qitUuZoS6y8LQo-5s2pPA-t1080x1080.jpg" alt="" />
+            <div className={style.col}>
+              <span className={style['album-title']}>My Playlist</span>
+              <span className={style['album-subtitle']}>Playlist • User</span>
+            </div>
+          </div>
+          <div className={style.item}>
+            <img className={style['album-cover']} width={40} src="https://i1.sndcdn.com/artworks-y6qitUuZoS6y8LQo-5s2pPA-t1080x1080.jpg" alt="" />
+            <div className={style.col}>
+              <span className={style['album-title']}>My Playlist</span>
+              <span className={style['album-subtitle']}>Playlist • User</span>
+            </div>
+          </div>
+          <div className={style.item}>
+            <img className={style['album-cover']} width={40} src="https://i1.sndcdn.com/artworks-y6qitUuZoS6y8LQo-5s2pPA-t1080x1080.jpg" alt="" />
+            <div className={style.col}>
+              <span className={style['album-title']}>My Playlist</span>
+              <span className={style['album-subtitle']}>Playlist • User</span>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
--- NJotify/src/components/PlayerBar.tsx
+++ NJotify/src/components/PlayerBar.tsx
@@ -1,11 +1,30 @@
 import style from '../styles/LayoutPage/PlayerBar.module.css'
+import { TbArrowsDiagonal } from "react-icons/tb";
+import { HiOutlineQueueList } from "react-icons/hi2";
+import { BsFilePlay } from "react-icons/bs";
+import { FiVolume2 } from "react-icons/fi";
+
+// import { PiAirplay } from "react-icons/pi"; //Alternatif BsFilePlay
+// import { PiAirplayFill } from "react-icons/pi";
+// import { PiArrowsOutSimpleBold } from "react-icons/pi"; //Ketebalan
 
 const PlayerBar = () => {
   return (
     <div className={`${style['container']} ${style['flex']}`}>
-      <h1>ASASA</h1>
-      <h1>ASASA</h1>
-      <h1>ASASA</h1>
+      <div className={style['player-info']}>
+          <img className={style['album-cover']} width={60} src="https://i1.sndcdn.com/artworks-y6qitUuZoS6y8LQo-5s2pPA-t1080x1080.jpg" alt="" />
+          <div className={style.col}>
+            <span className={style['album-title']}>My Playlist</span>
+            <span className={style['album-subtitle']}>Playlist • User</span>
+          </div>
+      </div>
+      <div className={`${style['flex']} ${style['gap']}`}>
+        <BsFilePlay/>
+        <HiOutlineQueueList/>
+        <FiVolume2/>
+        <input type="range" id="vol" name="vol" min="0" max="50"></input>
+        <TbArrowsDiagonal/>
+      </div>
     </div>
   );
 };
