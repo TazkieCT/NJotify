3ef85c4dde4c4613d39fdc156bee42e7add6e77a
Tazkie <100460377+TazkieCT@users.noreply.github.com>
2024-07-11 15:23:44 +0700

    Buat RightBar
    nambahin beberapa komponen belum selesai

--- NJotify/src/components/PlayerBar.tsx
+++ NJotify/src/components/PlayerBar.tsx
@@ -3,6 +3,10 @@ import { TbArrowsDiagonal } from "react-icons/tb";
 import { HiOutlineQueueList } from "react-icons/hi2";
 import { BsFilePlay } from "react-icons/bs";
 import { FiVolume2 } from "react-icons/fi";
+import { FaBackwardStep } from "react-icons/fa6";
+import { FaForwardStep } from "react-icons/fa6";
+import { IoIosPause } from "react-icons/io";
+import { FaPlay } from "react-icons/fa6";
 
 // import { PiAirplay } from "react-icons/pi"; //Alternatif BsFilePlay
 // import { PiAirplayFill } from "react-icons/pi";
@@ -12,11 +16,23 @@ const PlayerBar = () => {
   return (
     <div className={`${style['container']} ${style['flex']}`}>
       <div className={style['player-info']}>
-          <img className={style['album-cover']} width={60} src="https://i1.sndcdn.com/artworks-y6qitUuZoS6y8LQo-5s2pPA-t1080x1080.jpg" alt="" />
+          <img className={style['album-cover']} width={60} src="https://upload.wikimedia.org/wikipedia/id/thumb/c/c2/Radwimps_Your_Name_Album_Cover.jpg/220px-Radwimps_Your_Name_Album_Cover.jpg" alt="" />
           <div className={style.col}>
-            <span className={style['album-title']}>My Playlist</span>
-            <span className={style['album-subtitle']}>Playlist • User</span>
+            <span className={style['album-title']}>Sparkle - movie ver.</span>
+            <span className={style['album-subtitle']}>RADWIMPS</span>
           </div>
+      </div>
+      <div className={style['music-control']}>
+        <div className={`${style['flex']} ${style['center']}`}>
+          <FaBackwardStep/>
+          <span className={style['play-btn']}><FaPlay/></span>
+          <FaForwardStep/>
+        </div>
+        <div className={`${style['flex']} ${style['center']}`}>
+          <span>--:--</span>
+          <input type="range" id="progress" name="progress" min="0" max="50"></input>
+          <span>--:--</span>
+        </div>
       </div>
       <div className={`${style['flex']} ${style['gap']}`}>
         <BsFilePlay/>
--- NJotify/src/components/RightTab.tsx
+++ NJotify/src/components/RightTab.tsx
@@ -1,9 +1,41 @@
 import style from "../styles/LayoutPage/RightTab.module.css"
+import { IoCloseOutline } from "react-icons/io5";
+// import { HiOutlineDotsHorizontal } from "react-icons/hi";
+import { RxDotsHorizontal } from "react-icons/rx";
 
 const RightTab = () => {
   return (
     <div className={style.container}>
-
+      <div className={style['header-content']}>
+        <div className={`${style['flex-between']} ${style['sticky']}`}>
+          <a className={style['header-text']}>RADWIMPS</a>
+          <a className={style['header-button']}>
+            <span className={style.medium}><RxDotsHorizontal/></span>
+            <span className={style.medium}><IoCloseOutline/></span>
+            {/* <span className={style.medium}><IoArrowForward/></span> */}
+          </a>
+        </div>
+        <div className={style.content}>
+          <div className={style.album}>
+            <img className={style['album-cover']} src="https://upload.wikimedia.org/wikipedia/id/thumb/c/c2/Radwimps_Your_Name_Album_Cover.jpg/220px-Radwimps_Your_Name_Album_Cover.jpg" alt="" />
+            <span className={style.title}>Sparkle - movie ver.</span>
+            <span className={style.subtitle}>RADWIMPS</span>
+          </div>
+          {/* <div className={style.info}></div> */}
+          <div className={style.artist}>
+            <span className={style['about-artist']}>About the artist</span>
+            <div className={style.shadow}></div>
+            <img className={style['artist-image']} src="https://i.scdn.co/image/ab6761610000e5ebc9d443fb5ced1dd32d106632" alt="Artist" />
+            <div className={style['p-20']}>
+              <span className={style.white}>RADWIMPS</span>
+              <div className={`${style['between']} ${style['text-small']}`}>
+                <span className={style.subtitle}>x.xxx.xxx monthly listeners</span>
+                <span className={style['follow-btn']}>Follow</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
